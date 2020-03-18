6d9b1ac36733576251aca4c17419e6903532258b
James Cho <chojames500@gmail.com>
2020-03-18 18:27:02 -0500

    Player now looks at mouse position

---
+++ Shadow Tactics Mock/Assets/Scripts/CustomScripts.Entities/CustomScripts.Entities.PlayerSystem/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomScripts.Managers;
+
+namespace CustomScripts.Entities.PlayerSystem
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Start()
+        {
+            this.mainCamera = Camera.main;
+
+            UpdateManager.Instance.GlobalUpdate += this.LookAtMousePointer;
+        }
+
+        private Camera mainCamera;
+        private void LookAtMousePointer()
+        {
+            var mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCamera.transform.position.y);
+            mousePos = this.mainCamera.ScreenToWorldPoint(mousePos);
+            var lookAtPos = mousePos + Vector3.up * transform.position.y;
+            transform.LookAt(lookAtPos);
+        }
+    }
+}
---
+++ Shadow Tactics Mock/Assets/Scripts/CustomScripts.Managers/UpdateManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CustomScripts.Managers
+{
+    public class UpdateManager : MonoBehaviour
+    {
+        public static UpdateManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+
+        public event Action GlobalUpdate;
+        public event Action GlobalFixedUpdate;
+        public event Action GlobalLateUpdate;
+
+        private void Update()
+        {
+            this.GlobalUpdate?.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            this.GlobalFixedUpdate?.Invoke();
+        }
+
+        private void LateUpdate()
+        {
+            this.GlobalLateUpdate?.Invoke();
+        }
+    }
+}
